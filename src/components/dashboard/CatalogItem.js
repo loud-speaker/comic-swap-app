9cd3ccbd37bccb41257dbd25dccf13dac240c20e
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-19 23:42:29 -0700

    Added rendering for catalog items, comic data, etc.

--- src/components/dashboard/CatalogItem.js
+++ src/components/dashboard/CatalogItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from './CatalogItem.css';
 
 class CatalogItem extends Component {
 
@@ -13,9 +14,26 @@ class CatalogItem extends Component {
     console.log(catalogItem);
     
     return (
-      <section>
-        <h3>User CatalogItem Component</h3>
-        
+      <section className={styles.catalogItem}>
+        <h3>Current Catalog</h3>
+        <img src={catalogItem.comic.image}/>
+        <p>Issue: {catalogItem.comic.issueName}</p>
+        <p>Cover Date: {catalogItem.comic.coverDate}</p>
+        <p>Volume: {catalogItem.comic.volumeName}</p>
+        <p>Condition: {catalogItem.condition}</p>
+        <div dangerouslySetInnerHTML={{ __html: catalogItem.comic.description }}></div>
+        Characters:
+        <ul>
+          {catalogItem.comic.characters.map((character, i) => 
+            <li key={i}>{character}</li>  
+          )}
+        </ul>
+        Credits:
+        <ul>
+          {catalogItem.comic.personCredits.map(person => 
+            <li key={person._id}>{person.name} ({person.role})</li>  
+          )}
+        </ul>
       </section>
     );
   }
