aa5b6dacf9ca425e824252bbce40c3c23e93a0e5
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-20 22:41:54 -0700

    Zip codes and map updated

--- src/components/app/MapResults.js
+++ src/components/app/MapResults.js
@@ -2,17 +2,21 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getUser } from '../auth/reducers';
+import zipcodes from 'zipcodes';
 
 class MapResults extends PureComponent {
   static propTypes = {
     user: PropTypes.object,
   };
-
+  
+  
   render() {
     const { user } = this.props;
+    const userZipData = zipcodes.lookup(user.zip);
+    console.log(userZipData);
     return (
       <div>
-        <p>Check out some local comic stores in</p>
+        <p>Check out some local comic stores in {userZipData.city}, {userZipData.state}!</p>
         <iframe width="600" height="450" frameBorder="0" style={{ border: 0 }} src={'https://www.google.com/maps/embed/v1/search?q=comic%20book%20store%20near%20' + user.zip + '&key=AIzaSyBHViEFI_2ar58Eh4MNJFf-DJwUg2GaJfg'} allowFullScreen></iframe>
       </div>
     );
