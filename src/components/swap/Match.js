643a0aef5736fe2f1c44fadf0ab53df2f07d3d4b
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-21 13:51:36 -0700

    Fix guard clause

--- src/components/swap/Match.js
+++ src/components/swap/Match.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getUser } from '../auth/reducers';
@@ -14,37 +14,42 @@ class Match extends Component {
   
   render() { 
     const { match, user } = this.props;
-    const userZipData = zipcodes.lookup(match.user.zip);
+    let userZipData;
+    if(match.user.zip) userZipData = zipcodes.lookup(match.user.zip);
 
     return (
       <section className={styles.Match}>
-        <article>
-          <div className="userAvatar">
-            <img src={match.user.avatar}/>
-          </div>
-          <h2>User:</h2>
-          <p>{match.user.username}</p>
-          <a href={'mailto:' + user.email + '?subject=Comic-Swap Trade offer:' + ' ' + match.comic.issueName}><button>Email User</button></a>
-        </article>
+        {match.user.zip &&
+        <Fragment>
+          <article>
+            <div className="userAvatar">
+              <img src={match.user.avatar}/>
+            </div>
+            <h2>User:</h2>
+            <p>{match.user.username}</p>
+            <a href={'mailto:' + user.email + '?subject=Comic-Swap Trade offer:' + ' ' + match.comic.issueName}><button>Email User</button></a>
+          </article>
 
-        <div>
-          <h2 style={{ textAlign: 'center' }}>{match.user.username} is located in {userZipData.city}, {userZipData.state}</h2>
-          <iframe width="400" height="300" frameBorder="0" style={{ border: 0 }} src={'https://www.google.com/maps/embed/v1/search?q=comic%20book%20store%20near%20' + user.zip + '&key=AIzaSyBHViEFI_2ar58Eh4MNJFf-DJwUg2GaJfg'} allowFullScreen></iframe>
-        </div>
-        
-        <div className="comicCover">
-          <img src={match.comic.image}/>
-        </div>
-        <h2>Issue:</h2>
-        <p>{match.comic.issueName}</p>
-        <h2>Volume:</h2>
-        <p>{match.comic.volumeName}</p>
-        <h2>Cover Date:</h2>
-        <p>{match.comic.coverDate}</p>
-        <h2>Condition:</h2>
-        <h3>{match.comic.condition}</h3>
-        <h2>Status:</h2>
-        <h3>{match.comic.exchange}</h3>
+          <div>
+            <h2 style={{ textAlign: 'center' }}>{match.user.username} is located in {userZipData.city}, {userZipData.state}</h2>
+            <iframe width="400" height="300" frameBorder="0" style={{ border: 0 }} src={'https://www.google.com/maps/embed/v1/search?q=comic%20book%20store%20near%20' + user.zip + '&key=AIzaSyBHViEFI_2ar58Eh4MNJFf-DJwUg2GaJfg'} allowFullScreen></iframe>
+          </div>
+          
+          <div className="comicCover">
+            <img src={match.comic.image}/>
+          </div>
+          <h2>Issue:</h2>
+          <p>{match.comic.issueName}</p>
+          <h2>Volume:</h2>
+          <p>{match.comic.volumeName}</p>
+          <h2>Cover Date:</h2>
+          <p>{match.comic.coverDate}</p>
+          <h2>Condition:</h2>
+          <h3>{match.comic.condition}</h3>
+          <h2>Status:</h2>
+          <h3>{match.comic.exchange}</h3>
+        </Fragment>
+        }
       </section>
     );
   }
