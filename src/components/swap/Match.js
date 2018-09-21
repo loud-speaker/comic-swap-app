6d504e329884af26bdc343a5955b704e3a7838f2
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-21 13:05:32 -0700

    Final changes

--- src/components/swap/Match.js
+++ src/components/swap/Match.js
@@ -29,7 +29,7 @@ class Match extends Component {
 
         <div>
           <h2 style={{ textAlign: 'center' }}>{match.user.username} is located in {userZipData.city}, {userZipData.state}</h2>
-          <iframe width="267" height="200" frameBorder="0" style={{ border: 0 }} src={'https://www.google.com/maps/embed/v1/search?q=comic%20book%20store%20near%20' + user.zip + '&key=AIzaSyBHViEFI_2ar58Eh4MNJFf-DJwUg2GaJfg'} allowFullScreen></iframe>
+          <iframe width="400" height="300" frameBorder="0" style={{ border: 0 }} src={'https://www.google.com/maps/embed/v1/search?q=comic%20book%20store%20near%20' + user.zip + '&key=AIzaSyBHViEFI_2ar58Eh4MNJFf-DJwUg2GaJfg'} allowFullScreen></iframe>
         </div>
         
         <div className="comicCover">
