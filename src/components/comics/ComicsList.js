a608c4f367b9a3a1f1581ecd12f3bbd1920d6fe7
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-23 14:42:21 -0700

    Adjusted conditional loading and search results

--- src/components/comics/ComicsList.js
+++ src/components/comics/ComicsList.js
@@ -21,21 +21,23 @@ class ComicsList extends PureComponent {
 
   render() {
     const { comics, loading } = this.props;
+
     return (
       <div id="comicSearch">
         <SearchForm submit={this.handleSearch}/>
         {loading &&
           <Loading/>
         }
-        {comics.length > 0
-          ? <ComicsDisplay comics={comics}/>
-          : <h2>Search For Comics:</h2>
+        {comics.length > 0 && !loading &&
+          <ComicsDisplay comics={comics}/>
+        }
+        {comics.length < 1 && !loading &&
+          <h2>Search For Comics...</h2>
         }
       </div>
     );
   }
 }
-
 
 export default connect(
   state => ({
